cd1e41ac9ccf96ea8a4488cf10dd9a137d364d3d
pmalvido <pablo.malvidofresnillo@tuni.fi>
2023-03-11 01:07:41 +0200

    home visual appearance

--- src/views/Sensors.vue
+++ src/views/Sensors.vue
@@ -3,7 +3,9 @@
     <TitlePage page='Sensors'/>
   </div>
   <div class="content">
-  <button @click="publish_string('/topic_UI2',{msg})">Publish</button>
+    <button @click="publish_string('/topic_UI2',{msg})">Publish</button>
+    <button @click="publish_string_constant('/UI/mode','Idle')">Idle mode</button>
+    <button @click="publish_string_constant('/UI/mode','Running')">Running mode</button>
   <p>We and our partners use cookies to Store and/or access information on a device. We and our partners use data for Personalised ads and content, ad and content measurement, audience insights and product development. An example of data being processed may be a unique identifier stored in a cookie. Some of our partners may process your data as a part of their legitimate business interest without asking for consent. To view the purposes they believe they have legitimate interest for, or to object to this data processing use the vendor list link below. The consent submitted will only be used for data processing originating from this website. If you would like to change your settings or withdraw consent at any time, the link to do so is in our privacy policy accessible from our home page..</p>
   <label for="fname">User name:</label>
   <input type="text" v-model="username"><br><br>
@@ -49,6 +51,21 @@ export default {
 
       loadPublisher.publish(loadTopic);
     },
+
+    publish_string_constant(topic, message){
+      var loadPublisher = new ROSLIB.Topic({
+        ros : this.ros,
+        name : topic,
+        messageType : 'std_msgs/String'
+      });
+
+      var loadTopic = new ROSLIB.Message({
+          data: message
+      });
+
+      loadPublisher.publish(loadTopic);
+    },
+    
     create_user(){
       var encrypted = CryptoJS.AES.encrypt(this.password, this.password);
       var decrypted = CryptoJS.AES.decrypt(encrypted, this.password);
