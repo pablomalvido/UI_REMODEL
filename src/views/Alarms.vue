28f2cd1c6dc77f1a570970b5f4d8378f360ed30c
Saigopal Vasudevan (TAU) <saigopal.vasudevan@tuni.fi>
2023-03-20 17:00:23 +0200

    0320_Sai_Alarm_contact

--- src/views/Alarms.vue
+++ src/views/Alarms.vue
@@ -2,23 +2,26 @@
   <div>
       <TitlePage page='Alarms'/>
   </div>
-  <div id="activeAlarm">
-    
-  </div>
+  <!-- <div id="activeAlarm">
+    <h4>{{ rosCon }}</h4>
+  </div> -->
 </template>
 
 <script>
 import TitlePage from '@/components/Title_page.vue';
+import VueSidebarMenuAkahon from '@/components/Sidebar-menu-akahon.vue';
 
 export default {
     name: 'Alarms',
+    props: ['rosCon'],
 
     components: {TitlePage},
-    
+
 
     data(){
         return{
-            
+            rosCon: false,
+            listener_reset = null
             alarm_title: "ALARMS",
             active_alarms: [],
             inactive_alarms: [],
@@ -27,13 +30,54 @@ export default {
     },
 
     methods: {
+        // init_subscribers(){
+
+        // this.listener_reset = new ROSLIB.Topic({
+        //     ros : this.ros,
+        //     name : '/RSM/reset',  // /RSM/ROS_reset for tests
+        //     messageType : 'std_msgs/Bool' //Change this for your topic type
+        // });
+
+    //     this.rviz_image1_topic = new ROSLIB.Topic({
+    //     ros : this.ros,
+    //     name : '/camera1/image/compressed',
+    //     messageType : 'sensor_msgs/CompressedImage'
+    //   });
+
+    //   this.rviz_image1_topic.subscribe((message) => {
+    //     //console.log('RVIZ image 1 updated');
+    //     if (this.rviz_image1_topic.name == this.camera_selected){
+    //       this.last_time = Date.now();
+    //       this.rviz_image = "data:image/jpg;base64," + message.data;
+    //     }
+    //   });
+
+    //   this.rviz_image2_topic = new ROSLIB.Topic({
+    //     ros : this.ros,
+    //     name : '/camera2/image/compressed',
+    //     messageType : 'sensor_msgs/CompressedImage'
+    //   });
+
+    //   this.rviz_image2_topic.subscribe((message) => {
+    //     //console.log('RVIZ image 2 updated');
+    //     if (this.rviz_image2_topic.name == this.camera_selected){
+    //       this.last_time = Date.now();
+    //       this.rviz_image = "data:image/jpg;base64," + message.data;
+    //     }
+    //   });
+    },
+
+        // if(rosCon == false){
+        //         ros = new ROSLIB.Ros({
+        //             url : 'ws://192.168.43.168:9090' //Port = 9090
+        //         });
 
         mounted(){
             this.ros = new ROSLIB.Ros({
             url : 'ws://192.168.43.168:9090' //Port = 9090
             });
             console.log(this.ros)
-            
+
             this.ros.on('connection', () => {
             console.log('Connected to websocket server.');
             this.rosCon = true
@@ -61,7 +105,7 @@ export default {
 
 }
 
-    
+
 </script>
 
 <style>
